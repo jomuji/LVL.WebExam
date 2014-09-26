bbe40a246e56d2c0fea581546668b4c0dab76ff9
Jose Mujica <jose@netavantage.com>
2014-09-26 17:07:54 -0400

    ordre des items
    affichers les items recents

--- js/app.js
+++ js/app.js
@@ -1,23 +1,42 @@
-jQuery().ready(function ($) {
-    $.ajax({
-        url: 'http://examen.lvlnetworks.com/',
-        success: function (data) {
-            console.log(data);
-        },
-        error: function (err) {
-            console.log(err);
-        }
-    });
-});
+//jQuery().ready(function ($) {
+//    $.ajax({
+//        url: 'http://examen.lvlnetworks.com/',
+//        success: function (data) {
+//            console.log(data);
+//        },
+//        error: function (err) {
+//            console.log(err);
+//        }
+//    });
+//});
 
-var App = angular.module('App', []);
- 
-App.controller('ItemCtrl', function($scope, $http) {
-    $http.get('http://examen.lvlnetworks.com/')
-    .then(function(res){
-    $scope.items = res.data.Items;
-    });
-    setInterval($scope.items, 1000);
-});
 
+
+
+//var App = angular.module('App', ['datarefresh'])
+//
+//App.factory('itemsFactory', function($http) {
+//  return {
+//    getItemsAsync: function(callback) {
+//      $http.get('http://examen.lvlnetworks.com/').success(callback);
+//    }
+//  };
+//});
+//
+//App.controller('ItemCtrl', function($scope, itemsFactory) {
+//  itemsFactory.getItemsAsync(function(results) {
+//    //console.log('ItemCtrl async returned value');
+//    $scope.items = results.Items;
+//      
+//  });
+//    
+//});
+
+
+angular.module('App',['datarefresh'])
+.controller('ItemCtrl', [function () {
+}]);
+
+       
+    
 
